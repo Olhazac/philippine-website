6529a91e59335f59b21211dfded3b2fdcb9f4cd7
Olhazac <zacharybrindley123@gmail.com>
2022-06-15 17:46:59 +1000

    first commit

---
+++ components/Masthead.js
@@ -0,0 +1,6 @@
+export default function Masthead() {
+    return <div className="min-h-screen flex flex-col items-center justify-center">
+    <h1>Welcome to the Philippines</h1>
+    <h6 className="text-xs">Beauty, defined.</h6>
+    </div>
+}
---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return <nav className="bg-black">
+        <div className="text-white">
+        <h1 className="inline-block text-2xl p-6">Philippines</h1>
+        <a className="no-underline p-4">Flights</a>
+        <a className="no-underline p-4">Places to Go</a>
+        </div>
+    </nav>
+
+}
